a8b4349745f24b34d12dfce408e066d948571a00
Sebin Nam <nsb2519@naver.com>
2016-03-28 20:52:52 +0900

    #5 implement TextView Result Activity

--- app/src/main/java/kr/hs/emirim/sebin2519/monchicken/ResultActivity.java
+++ app/src/main/java/kr/hs/emirim/sebin2519/monchicken/ResultActivity.java
@@ -3,15 +3,25 @@ package kr.hs.emirim.sebin2519.monchicken;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 /**
  * Created by Mirim on 2016-03-28.
  */
 public class ResultActivity extends AppCompatActivity {
 
+    TextView mResult;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mResult=(TextView)findViewById(R.id.result);
+        mResult.setText("결과");
+
+
     }
+
+
 }

c9b0fbc9b74266a365e6ba2fed0b242287dc1c00
Sebin Nam <nsb2519@naver.com>
2016-03-28 20:44:30 +0900

    #3 implements input user

--- app/src/main/java/kr/hs/emirim/sebin2519/monchicken/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/sebin2519/monchicken/MainActivity.java
@@ -2,12 +2,23 @@ package kr.hs.emirim.sebin2519.monchicken;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText mName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mName = (EditText)findViewById(R.id.name);//activity_main에서 가져와서 사용 / 타입캐스팅.;
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mName.setText("");
     }
 }

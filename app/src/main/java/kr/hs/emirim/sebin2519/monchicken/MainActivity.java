6ccf08a32f0f410fbe3183507f729e41af7c685e
SeBin Nam <nsb2519@naver.com>
2016-04-12 20:06:43 +0900

    #11 add Animation code_Main to Result

--- app/src/main/java/kr/hs/emirim/sebin2519/monchicken/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/sebin2519/monchicken/MainActivity.java
@@ -21,7 +21,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         mName = (EditText)findViewById(R.id.name);//activity_main에서 가져와서 사용 / 타입캐스팅.;
         mBotton=(Button) findViewById(R.id.btn_show_me_the_chicken);
-        mBotton.setOnClickListener(this);//main_activity는 클릭을 담당할 거에요!
+        mBotton.setOnClickListener(this);//main_activity는 클릭을 담당할 거에요!, 이벤트 핸들러 마스터(핸들러, 처리자)
     }
 
     @Override
@@ -36,13 +36,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
      * @param v The view that was clicked.
      */
     @Override
-    public void onClick(View v) {
+    public void onClick(View v) { //클릭했을때 자동으로 이 메서드를 실행
         //누른 순간에 입력한것을 받아옴
         String name=mName.getText().toString();//내용을 읽어오세요: getText() - 단 retun타입이 Editable => toString으로 반환해주어야함
 
         //name 출력
 //        if(name==null) {//이름이 NULL일 경우 예외처리작업
-//            Toast.makeText(this,"이름을 입력해 주세요!",Toast.LENGTH_LONG).show();
+//            Toast.makeText(th/이벤트 핸들러 클래스is,"이름을 입력해 주세요!",Toast.LENGTH_LONG).show();
 //        }else {
 //            Toast.makeText(this, name + "씨 배고파요!(ㅜ. ㅜ)", Toast.LENGTH_LONG).show(); //상대방이 이름을 입력하지 않을경우 이상한 것이 뜸 => 예외처리해주어야함
 //            //Toast: 밑에 잠깐 나왔다가 사라지는 것(반드시 메소드가 있어야함 / 여러개의 메소드 연결: 체인 메서드
@@ -56,7 +56,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             intent.putExtra("username",name);//Extra: 추가
             intent.putExtra("age",10);//나이 넘기기
             startActivity(intent);
-            overridePendingTransition(0, R.anim.zoom_exit);
+            overridePendingTransition(R.anim.slide_in_right,R.anim.slide_out_left);
         }catch(NullPointerException e){
             Toast.makeText(this,"이름을 입력해 주세요!",Toast.LENGTH_LONG).show();
         }catch(Exception e) { //모든 예외(나머지 예외)
